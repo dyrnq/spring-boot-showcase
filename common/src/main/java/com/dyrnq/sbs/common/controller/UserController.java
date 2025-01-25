eee3d51719f010aa934c6ddea53946fc6bc8809e
dyrnq <dyrnq@qq.com>
2025-01-25 15:44:29 +0800

    Update UserController.java

--- common/src/main/java/com/dyrnq/sbs/common/controller/UserController.java
+++ common/src/main/java/com/dyrnq/sbs/common/controller/UserController.java
@@ -2,6 +2,7 @@ package com.dyrnq.sbs.common.controller;
 
 import com.dyrnq.sbs.common.dto.ResponseDTO;
 import com.dyrnq.sbs.common.model.UserModel;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class UserController {
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.ALL_VALUE)
-    public ResponseDTO<UserModel> add(@ModelAttribute UserModel userModel) {
+    public ResponseDTO<UserModel> add(@ModelAttribute UserModel userModel, HttpServletRequest request) {
         ResponseDTO<UserModel> responseDTO = new ResponseDTO<UserModel>();
+        responseDTO.setMetaData(request.getHeader("Content-Type"));
         responseDTO.setData(userModel);
         return responseDTO;
     }
 
 
     @RequestMapping(value = "/add-body", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.ALL_VALUE)
-    public ResponseDTO<UserModel> addBody(@RequestBody UserModel userModel) {
+    public ResponseDTO<UserModel> addBody(@RequestBody UserModel userModel, HttpServletRequest request) {
         ResponseDTO<UserModel> responseDTO = new ResponseDTO<UserModel>();
+        responseDTO.setMetaData(request.getHeader("Content-Type"));
         responseDTO.setData(userModel);
         return responseDTO;
     }
